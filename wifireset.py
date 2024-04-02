81693981448e870f1cd44290b31f3049a7e1f890
GabDrago <gabriele.drago03@gmail.com>
2024-04-02 23:52:53 +0200

    prima versione bot

--- wifireset.py
+++ wifireset.py
@@ -11,10 +11,10 @@ from change_wifi_pwd import change_wifi_pwd
 
 currDir = os.path.dirname(__file__)
 
-def parser(key : str):
-    idx = lines.find(key)
-    idx2 = lines.find("\n", idx)
-    s = lines[idx:idx2]
+def parser(key : str, stringa : str):
+    idx = stringa.find(key)
+    idx2 = stringa.find("\n", idx)
+    s = stringa[idx:idx2]
     return s.split(" ")[1]
 
 
@@ -22,9 +22,9 @@ lines = ""
 with open(os.path.join(currDir, ".passwords.txt"), "r") as f:
     lines = f.read()
 
-SSID = parser("SSID")
-pwd1 = parser("pwd1")
-pwd2 = parser("pwd2")
+SSID = parser("SSID", lines)
+pwd1 = parser("pwd1", lines)
+pwd2 = parser("pwd2", lines)
 
 
 change_local_pwd(SSID, pwd2)
